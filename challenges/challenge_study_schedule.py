84145e64a63a826ee82aa30035541567d25d2584
ricardo-sousa <rwmsousa@gmail.com>
2022-04-20 17:07:31 -0300

    feat: requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,27 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    frequency = 0
+
+    if target_time is None:
+        return None
+
+    for entrance, exit_time in permanence_period:
+        if type(entrance) != int or type(exit_time) != int:
+            return None
+        if entrance <= target_time <= exit_time:
+            frequency += 1
+    return frequency
+
+# Exemplo:
+
+# Nos arrays temos 6 estudantes
+
+# estudante             1       2       3       4       5       6
+# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+
+# target_time = 5  # saída: 3, pois a quarta, a quinta e a sexta pessoa estudante ainda estavam estudando nesse horário.
+# target_time = 4  # saída: 3, pois a quinta e a sexta pessoa estudante começaram a estudar nesse horário e a quarta ainda estava estudando.
+# target_time = 3  # saída: 2, pois a terceira e a quarta pessoa estudante ainda estavam estudando nesse horário.
+# target_time = 2  # saída: 4, pois a primeira, a segunda, a terceira e a quarta pessoa estudante estavam estudando nesse horário.
+# target_time = 1  # saída: 2, pois a segunda e a quarta pessoa estudante estavam estudando nesse horário.
+
+# Para esse exemplo, depois de rodar a função para todos esses `target_times`, julgamos que o melhor horário é o `2`, pois esse retornou `4`, já que 4 estudantes estavam presentes nesse horário!
